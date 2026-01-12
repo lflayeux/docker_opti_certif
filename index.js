bbaa0fb5ee5c3666efff4f6d6876ca3f706c354c
lflayeux <lflayeux@student.42.fr>
2026-01-12 10:10:27 +0100

    test: v1.3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World! de merde de merde</h1>')
+  res.send('<h1>Hello World! ca y est ca va marcher je penses</h1>')
 })
 
 const PORT = 8080
